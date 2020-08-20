da30a735b5095c2919b455de3b141b23f0cf3c42
Kate McKenzie <kate.j.mckenzie@gmail.com>
2020-08-20 12:10:59 +1200

    Test basic Subscriber email address creation

--- newsletter/tests.py
+++ newsletter/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Subscriber
+
+
+class SubscriberModelTests(TestCase):
+
+    def test_subscriber_email_address(self):
+        subscriber = Subscriber(email_address='test@test.com')
+        self.assertIs(subscriber.email_address, 'test@test.com')
