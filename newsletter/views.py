1c9fabd71091c62f5f112af2cc80ab71f06e54ec
Kate McKenzie <kate.j.mckenzie@gmail.com>
2020-08-24 10:48:33 +1200

    Add static routing to first subscriber Fix import bug

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from models import Subscriber
+from .models import Subscriber
 
 
 def subscriber_list():
--- tinypage/urls.py
+++ tinypage/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from newsletter.views import subscriber_get
+
 urlpatterns = [
+    path('subscriber/1/', subscriber_get),
     path('admin/', admin.site.urls),
 ]
