fc117a14b17b61f39f563bad54541f8e4481117b
Kate McKenzie <kate.j.mckenzie@gmail.com>
2020-08-26 09:18:08 +1200

    Add subscriber retrieval page Add static image handling

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import Http404
 
 from .models import Subscriber
 
@@ -10,11 +10,11 @@ def subscriber_list():
 
 def subscriber_get(request, id_=None, *args, **kwargs):
     try:
-        obj = Subscriber.objects.get(id=id_)
+        subscriber = Subscriber.objects.get(id=id_)
     except Subscriber.DoesNotExist:
         raise Http404
 
-    return HttpResponse(f'<h1>Hello {obj}</h1>')
+    return render(request, 'get.html', {'subscriber': subscriber})
 
 
 def subscriber_create():
