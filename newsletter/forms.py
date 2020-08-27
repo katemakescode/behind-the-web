77601bb94eac04cd590abc327ee8abee37327be6
Kate McKenzie <kate.j.mckenzie@gmail.com>
2020-08-27 13:18:41 +1200

    Add custom error message for pre-existing email address

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -7,3 +7,10 @@ class SubscriberForm(forms.ModelForm):
     class Meta:
         model = Subscriber
         fields = ['email_address']
+
+    def clean_email_address(self):
+        email_address = self.cleaned_data.get("email_address")
+        qs = Subscriber.objects.filter(email_address__iexact=email_address)
+        if qs.exists():
+            raise forms.ValidationError("You have already subscribed")
+        return email_address
