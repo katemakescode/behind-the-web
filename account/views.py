bcccfa6a6a99cad19f7ec9b668b4d05a2a726dce
Kate McKenzie <kate.j.mckenzie@gmail.com>
2020-08-28 10:59:29 +1200

    Add landing page

--- account/views.py
+++ account/views.py
@@ -1,3 +1,4 @@
+from django.contrib import auth
 from django.shortcuts import render, redirect
 
 from .forms import RegistrationForm, LoginForm
@@ -7,6 +8,14 @@ def login(request):
     form = LoginForm(request.POST or None)
 
     if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+
+        user = auth.authenticate(request, username=username, password=password)
+        if not user:
+            return redirect('account/login.html')
+
+        auth.login(request, user)
         return redirect('/')
 
     return render(request, 'account/login.html', {'form': form})
--- tinypage/urls.py
+++ tinypage/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('', include('home.urls')),
     path('account/', include('account.urls')),
     path('newsletter/', include('newsletter.urls')),
     path('admin/', admin.site.urls),
