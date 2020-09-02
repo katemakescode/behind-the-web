cc22fa11fe2657e6c59c8f65306d3b3da7438099
Kate McKenzie <kate.j.mckenzie@gmail.com>
2020-09-03 10:11:29 +1200

    Add session list template and view

--- tinypage/overtrick/urls.py
+++ tinypage/overtrick/urls.py
@@ -5,4 +5,10 @@ from . import views
 app_name = 'overtrick'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('sessions/', views.session_list, name='sessions-list'),
+    path(
+        'sessions/<int:session_id>',
+        views.session_detail,
+        name='sessions-detail'
+    ),
 ]
--- tinypage/overtrick/views.py
+++ tinypage/overtrick/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
+from .models import Session
+
 
 def index(request):
+    return render(request, 'overtrick/index.html')
+
+
+def session_list(request):
+    sessions = Session.objects.all()
+    return render(
+        request,
+        'overtrick/session/list.html',
+        {'sessions_list': sessions}
+    )
+
+
+def session_detail(request):
     return render(request, 'overtrick/index.html')
