163bab9ca39fd5bf05d904f35dfc49cc3b35f655
Kate McKenzie <kate.j.mckenzie@gmail.com>
2020-09-17 08:18:56 +1200

    Bootstrapify contact form Remove suffix from field labels

--- tinypage/contact/forms.py
+++ tinypage/contact/forms.py
@@ -5,3 +5,7 @@ class ContactForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     message = forms.CharField(widget=forms.Textarea)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ''
