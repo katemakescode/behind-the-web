4589570c66801c95b65b1a89b7685e8fc4d9a14e
Kate McKenzie <kate.j.mckenzie@gmail.com>
2020-09-23 13:17:49 +1200

    Add initial home page tests

--- tinypage/contact/tests.py
+++ tinypage/contact/tests.py
@@ -1,10 +1,2 @@
 from django.test import TestCase
 
-from .models import Subscriber
-
-
-class SubscriberModelTests(TestCase):
-
-    def test_subscriber_email_address(self):
-        subscriber = Subscriber(email_address='test@test.com')
-        self.assertIs(subscriber.email_address, 'test@test.com')
